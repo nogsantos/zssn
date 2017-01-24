9c1e7d94e0ccdfd9cab2665f043b396391ad5787
Fabricio Nogueira <nogsantos@gmail.com>
2017-01-23 23:30:06 -0200

    Dev an auth form

---
+++ src/resources/elements/account/auth-form.ts
@@ -0,0 +1,16 @@
+import { autoinject, customElement, bindable } from 'aurelia-framework';
+/**
+ * @description
+ *  Map as element
+ * @namespace 
+ *  Elements
+ * 
+ * @author Fabricio Nogueira
+ */
+@customElement('auth-form')
+@autoinject()
+export class AuthForm {
+    constructor(parameters) {
+        
+    }
+}
